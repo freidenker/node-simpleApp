87864a212f13d0f5b8e9933b6e514e98e13aa380
winter <chao.lin2@hpe.com>
2017-03-20 17:08:55 +0800

    update db settings

--- settings.js
+++ settings.js
@@ -1,7 +1,8 @@
 module.exports = {
   cookieSecret: 'myblog',
   db: 'blog',
-  host: '15.107.17.82',
+//  host: '15.107.17.82',
 //  host: 'localhost',
+  host: '54.202.53.55',  
   port: 27017
 };
