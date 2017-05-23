6e5c57e7b70155bc7a35fe46d7d54db135df032b
Lin Chao <elysianfieldslc@hotmail.com>
2017-05-23 18:07:51 +0800

    update server.js for test of jenkins

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World, Ich bin Winter!')
+  res.send('Hello World, Ich bin Winter, update at 2017.5.23!')
 })
 
 app.listen(3000, function () {
