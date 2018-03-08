29123e662fb633e14c261be47c6464e69459b736
Lin Chao <clin@hpe.com>
2018-03-08 13:25:12 +0800

    update groovy

--- sync-template.groovy
+++ sync-template.groovy
@@ -1,13 +1,25 @@
 #!groovys
+def aws_account_id
 
 pipeline {
 
     agent { label 'master' }
+    parameters {
+      choice(choices: 'sigma\nsaw', description: 'aws account', name: 'awsAccount')
+    }
     stages {
 
-        stage('stage #2: get the list'){
+        stage('tell the account of aws'){
             steps {
-                    sh "echo hallo world sommer zwei drei vier funf"
+              script{
+                if(params.awsAccount=='saw'){
+                    aws_account_id="saw"
+                    sh "echo saw account"
+                }else if(params.awsAccount=='sigma'){
+                    aws_account_id="sigma"
+                    sh "echo sigma account"
+                }
+              }
             }
         }
 
