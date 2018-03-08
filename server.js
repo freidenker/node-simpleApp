5d3d3ad002eca931f269447eede155effe49ebf6
Lin Chao <clin@hpe.com>
2018-03-08 09:53:31 +0800

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hallo World, Ich bin Winter, update at 2018.03.08!')
+  res.send('Hallo DEUTSCHLAND, Ich bin Winter, update at 2018.03.08!')
 })
 
 app.listen(3000, function () {
