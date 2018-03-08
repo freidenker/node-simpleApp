d82b6da4d933f6d4c3e11c52a9e7777fa1c676d3
Lin Chao <clin@hpe.com>
2018-03-08 11:24:02 +0800

    update groovy

--- sync-template.groovy
+++ sync-template.groovy
@@ -11,7 +11,7 @@ pipeline {
 
         stage('stage #2: get the list'){
             steps {
-                    sh "echo hallo world sommer zwei"
+                    sh "echo hallo world sommer zwei drei"
             }
         }
 
