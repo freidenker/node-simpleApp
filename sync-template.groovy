bd556b3b5b6445b6265f88c52ea4d0e2d53da99e
Lin Chao <clin@hpe.com>
2018-03-08 11:11:38 +0800

    add groovy

--- sync-template.groovy
+++ sync-template.groovy
@@ -11,7 +11,7 @@ pipeline {
 
         stage('stage #2: get the list'){
             steps {
-                    sh "echo hallo"
+                    sh "echo hallo world"
             }
         }
 
